792cfbca6bdcb476e390428a6d4afd9bb38341c4
John Campbell <j.a.campbell@icloud.com>
2017-01-09 11:48:37 +0000

    First commit - basket and item classes

---
+++ basket/Basket.java
@@ -0,0 +1,18 @@
+package basket;
+import java.util.*;
+import java.io.Console;
+
+public class Basket{
+  private String name;
+  private ArrayList<Item> items;
+  private ArrayList<Discount> discounts;
+
+public Basket(){
+  this.name = "my_basket"
+  this.items = new ArrayList<Item>();
+  this.discounts = new ArrayList<Discount>();
+  }
+
+
+
+}
---
+++ basket/Item.java
@@ -0,0 +1,22 @@
+package basket;
+import java.util.*;
+import java.io.Console;
+
+public class Item{
+  private String name;
+  private Float price;
+
+public Item(String name; Float price){
+  this.name = name;
+  this.price = price;
+}  
+
+public String getName(){
+  return this.name;
+}
+
+public Float getPrice(){
+  return this.price;
+}
+
+}
